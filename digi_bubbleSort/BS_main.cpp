649261a87f5e0d9ac5aa12b976e6b5cdac348fa3
devdigia <devdigia@uat.edu>
2018-07-18 17:23:53 -0700

    Add project files.

---
+++ digi_bubbleSort/BS_main.cpp
@@ -0,0 +1,16 @@
+// digi_bubbleSort.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "BubbleSort.h"
+#include <iostream>
+
+
+int main()
+{
+	runBubbleSort();
+	system("pause");
+
+	return 0;
+}
+
---
+++ digi_bubbleSort/BubbleSort.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+void swap(int *xp, int *yp);
+void bubbleSort(int[], int n);
+void printArray(int arr[], int size);
+int runBubbleSort();
